c177dad6b9f7e963eb0199d847e0d2662ce8ddf0
KidChaos <genesis.enedeh@gmail.com>
2021-12-22 20:13:11 +0100

    put in two new models for the books app

--- IBC/books/admin.py
+++ IBC/books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Book, BookType
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(BookType)
--- IBC/books/models.py
+++ IBC/books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class BookType(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.TextField(max_length=200, help_text='Max text length is 200')
+    books = models.ManyToManyField('Book')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(unique=True, primary_key=True)
+    stars = models.SmallIntegerField()
+    story = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
