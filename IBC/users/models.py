adc7a59e8c55bbc11a0e9a2c048160433c292711
KidChaos <genesis.enedeh@gmail.com>
2021-12-22 20:50:00 +0100

    put in a new model in users app

--- IBC/users/models.py
+++ IBC/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=8, unique=True, help_text='Max of 8 Characters')
+    image = models.ImageField()
+    date_of_birth = models.DateField()
+    slug = models.SlugField(primary_key=True)
+    gender = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    level = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.username}"
